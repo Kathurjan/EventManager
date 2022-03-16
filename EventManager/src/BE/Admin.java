6fc8980192d157e4758afac276151a439a7522d7
Kathurjan <kath097c@easv365.dk>
2022-03-16 16:06:09 +0100

    BE admin created along with some scenbuilder stuff

--- EventManager/src/BE/Admin.java
+++ EventManager/src/BE/Admin.java
@@ -1,4 +1,44 @@
 package BE;
 
 public class Admin {
+    private String Username;
+    private String Password;
+    private String name;
+
+
+    public Admin(String username, String password, String name) {
+        Username = username;
+        Password = password;
+        this.name = name;
+    }
+
+
+
+
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
--- EventManager/src/Main.java
+++ EventManager/src/Main.java
@@ -1,6 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/Gui/view/Login.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Attendance System");
+
+        primaryStage.show();
+    }
+    static void main(String[] args){
+
 
     }
 }
