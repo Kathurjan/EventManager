8da125a3245b712136c1eae018c6853b4af3d8a2
Kathurjan <kath097c@easv365.dk>
2022-03-16 16:29:19 +0100

    FX stuff

--- EventManager/src/Gui/Controller/LoginController.java
+++ EventManager/src/Gui/Controller/LoginController.java
@@ -1,4 +1,19 @@
 package Gui.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+    public void signIn(ActionEvent actionEvent) throws IOException {
+        Parent part = FXMLLoader.load(getClass().getResource("/Gui/view/Classroom3.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(part);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
