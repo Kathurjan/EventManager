cdb553d33826b0e1bab480def7ec0e33f4bc119c
Kathurjan <kath097c@easv365.dk>
2022-03-17 20:34:11 +0100

    work on login controller nothing major

--- EventManager/src/Gui/Controller/LoginController.java
+++ EventManager/src/Gui/Controller/LoginController.java
@@ -1,19 +1,30 @@
 package Gui.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class LoginController {
-    public TextField UserName;
-    public PasswordField PassWord;
+public class LoginController implements Initializable {
+    @FXML
+    private TextField UserName;
+    @FXML
+    private PasswordField PassWord;
+    @FXML
+    private Button closeButton;
+
+
 
 
     public void signIn(ActionEvent actionEvent) throws IOException {
@@ -23,6 +34,17 @@ public class LoginController {
         Scene scene = new Scene(part);
         stage.setScene(scene);
         stage.show();
+    }
+
+
+    public void close(ActionEvent actionEvent) {
+        Stage stage =  (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 
 
