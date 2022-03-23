9dd4c2427e8bbc34dbf19ebfbc2aa8242c3b6834
Kathurjan <kath097c@easv365.dk>
2022-03-23 18:01:41 +0100

    Ignore this, just name changes

--- EventManager/src/DAL/EventDAO.java
+++ EventManager/src/DAL/EventDAO.java
@@ -11,15 +11,15 @@ public class EventDAO {
         con = new DatabaseConnector();
     }
 
-    public void addEvent(int eventID, String eventname, Date startdate, String eventlocation, double price) {
+    public void addEvent(int eventID, String eventName, Date startDate, String eventLocation, double price) {
         String sqlQuery = "INSERT INTO Event(ID, EventName, Startdate, EventLocation,event price) ";
 
         try(Connection db = con.getConnection()) {
             PreparedStatement statement = db.prepareStatement(sqlQuery);
             statement.setInt(1,eventID);
-            statement.setString(2,eventname);
-            statement.setDate(3, (java.sql.Date) startdate);
-            statement.setString(4,eventlocation);
+            statement.setString(2,eventName);
+            statement.setDate(3, (java.sql.Date) startDate);
+            statement.setString(4,eventLocation);
             statement.setDouble(5,price);
 
         } catch (SQLException e) {
@@ -27,4 +27,7 @@ public class EventDAO {
         }
 
     }
+
+
+
 }
