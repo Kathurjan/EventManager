b9f3bcc2b1b2b06c96b1afb7aeb164f1d2a610b1
MadsMGrin <business.madsmadsen@gmail.com>
2022-03-24 18:22:42 +0100

    population

--- EventManager/src/Gui/Model/EventModel.java
+++ EventManager/src/Gui/Model/EventModel.java
@@ -1,19 +1,18 @@
 package Gui.Model;
 
-import BLL.BLLFacade;
 import BLL.BLLInterface;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 
 import java.util.Date;
 
 public class EventModel {
-    private BLLInterface bllfacade;
+    private BLLInterface BLLInterface;
 
     public EventModel() throws SQLServerException {
-        bllfacade = new BLLFacade();
+        BLLInterface = new BLLInterface();
     }
 
     public void addEvent(int eventID, String eventName, Date startDate, String eventLocation, double price, String startTime){
-    bllfacade.addEvent(eventID,eventName,startDate,eventLocation,price,startTime);
+        BLLInterface.addEvent(eventID,eventName,startDate,eventLocation,price,startTime);
     }
 }
