9439c9ffc507ba0973a2e5d1ed72721926a8fbb8
Filip <91468087+Neshi24@users.noreply.github.com>
2022-03-29 09:16:23 +0200

    New user v1

---
+++ EventManager/src/Gui/Controller/NewUserCreationController.java
@@ -0,0 +1,21 @@
+package Gui.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class NewUserCreationController {
+
+
+    @FXML
+    private TextField userNameTxt, emailTxt, password1stTxt, password2ndTxt;
+
+    public void onAddUserBTNPress(ActionEvent actionEvent) {
+
+    }
+
+    public void onCancelBTNPress(ActionEvent actionEvent) {
+    }
+}
